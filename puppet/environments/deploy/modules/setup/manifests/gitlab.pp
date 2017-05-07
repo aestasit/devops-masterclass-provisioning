70afb1529a2c986289ad69d5e1406868d249f032
Andrey Adamovich <andrey@aestasit.com>
2017-05-07 10:19:22 +0300

    setup gitlab in docker

--- puppet/environments/deploy/modules/setup/manifests/gitlab.pp
+++ puppet/environments/deploy/modules/setup/manifests/gitlab.pp
@@ -1,19 +1,39 @@
 
 class setup::gitlab(
-
+  $gitlab_version = '9.1.2-ce.0'
 ) {
 
-  #
-  # TODO: implement
-  #
-  # class { 'gitlab':
-  #   external_url => 'http://gitlab.extremeautmation.io',
-  # }
-  #
+  file { [
+    '/var/lib/gitlab',
+    '/var/lib/gitlab/config',
+    '/var/lib/gitlab/logs',
+    '/var/lib/gitlab/data',
+  ]:
+    ensure => directory
+  }
+
+  docker::run { 'gitlab':
+    image            => "gitlab/gitlab-ce:${gitlab_version}",
+    ports            => [
+      '8443:443',
+      '8480:80',
+      '8422:22'
+    ],
+    restart_service  => true,
+    volumes          => [
+      '/var/lib/gitlab/config:/etc/gitlab',
+      '/var/lib/gitlab/logs:/var/log/gitlab',
+      '/var/lib/gitlab/data:/var/opt/gitlab',
+    ],
+    extra_parameters => [
+      '--restart=always',
+      '--hostname gitlab.extremeaution.io'
+    ],
+  }
 
   nginx::resource::server { 'gitlab.extremeautomation.io':
     listen_port => 80,
-    proxy       => 'http://localhost:8081',
+    proxy       => 'http://localhost:8480',
   }
 
 }
