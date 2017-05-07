1cf5fbb30c70d46a7f4f67970d5b6a032670a60e
Andrey Adamovich <andrey@aestasit.com>
2017-05-07 11:53:16 +0300

    added swarm init and docker registry

--- puppet/environments/deploy/modules/setup/manifests/docker.pp
+++ puppet/environments/deploy/modules/setup/manifests/docker.pp
@@ -1,51 +1,52 @@
-
 class setup::docker {
 
   class { '::docker':
-    package_name => 'docker-ce',
+    package_name                => 'docker-ce',
     use_upstream_package_source => false
   }
 
-  # TODO; docker swarm
+  exec { 'docker swarm init':
+    command     => "docker swarm init",
+    environment => 'HOME=/root',
+    path        => ['/bin', '/usr/bin'],
+    timeout     => 0,
+    unless      => 'docker info | grep -w "Swarm: active"',
+    require     => Class['::docker']
+  }
 
-  #   $docker_swarm_init_flags = docker_swarm_init_flags({
-#     init => $init,
-#     advertise_addr => $advertise_addr,
-#     autolock => $autolock,
-#     cert_expiry => $cert_expiry,
-#     dispatcher_heartbeat => $dispatcher_heartbeat,
-#     external_ca => $external_ca,
-#     force_new_cluster => $force_new_cluster,
-#     listen_addr => $listen_addr,
-#     max_snapshots => $max_snapshots,
-#     snapshot_interval => $snapshot_interval,
-#   })
-#
-#   $exec_init = "${docker_command} ${docker_swarm_init_flags}"
-#   $unless_init = 'docker info | grep -w "Swarm: active"'
-#
-#   exec { 'Swarm init':
-#     command     => $exec_init,
-#     environment => 'HOME=/root',
-#     path        => ['/bin', '/usr/bin'],
-#     timeout     => 0,
-#     unless      => $unless_init,
-#   }
-# }
-#
+  file { '/var/lib/registry':
+    ensure => directory
+  }
 
-  # TODO: docker registry
+  file { [
+    '/etc/docker/registry',
+    '/etc/docker/registry/certs'
+  ]:
+    ensure => directory
+  }
 
-  # docker run -d -p 5000:5000 --restart=always --name registry \
-  # -v `pwd`/data:/var/lib/registry \
-  # registry:2
-  #
-  # docker run -d -p 5000:5000 --restart=always --name registry \
-  # -v `pwd`/certs:/certs \
-  # -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
-  # -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
-  # registry:2
+  # TODO: add certificates
 
+  docker::run { 'registry':
+    image            => "registry:2",
+    ports            => [ '5000:5000' ],
+    # env              => [
+    #   "REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt",
+    #   "REGISTRY_HTTP_TLS_KEY=/certs/domain.key"
+    # ],
+    restart_service  => true,
+    volumes          => [
+      "/var/lib/registry:/var/lib/registry",
+      "/etc/docker/registry/certs:/certs",
+    ],
+    extra_parameters => [
+      '--restart=always',
+    ],
+    require          => [
+      File['/var/lib/registry'],
+      File['/etc/docker/registry/certs']
+    ]
+  }
 
   # TODO: add virtual host for registry??
 
