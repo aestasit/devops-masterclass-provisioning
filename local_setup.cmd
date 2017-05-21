a8ecc36f9396eb1a96819ae099f6a93243f5b745
Andrey Adamovich <andrey@aestasit.com>
2017-05-22 00:28:33 +0300

    updated local windows setup

--- local_setup.cmd
+++ local_setup.cmd
@@ -1,7 +1,9 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco install putty.install --force -y --allowEmptyChecksums
+choco install kitty
 choco install virtualbox --force -y
 choco install vagrant --force -y --allowEmptyChecksums
 choco install packer --force -y
 choco install awscli --force -y
 choco install git.install --force -y
+choco install python --version 2.7.6
